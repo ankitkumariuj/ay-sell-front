31f8d1fc8bbe83511f14e0e474fb672ef58b552a
ANKIT KUMAR <153356039+ankitkumariuj@users.noreply.github.com>
2025-01-27 09:17:32 +0530

    Update Adminlogin.jsx

--- src/AddData/Adminlogin.jsx
+++ src/AddData/Adminlogin.jsx
@@ -14,7 +14,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await axios.post('https://ay-sell-back.onrender.com/api/adminregister/login', { email, password });
+      const response = await axios.post('http://localhost:5000/api/adminregister/login', { email, password });
       localStorage.setItem('adminToken', response.data.token);
      
         // Store token in localStorage
@@ -56,7 +56,7 @@ function Login() {
             required
           />
         </div>
-        <button type="submit">Login</button>
+        <button type="submit" style={{marginLeft: '0px', width: '100%'}}>Login</button>
       </form>
 
       {successMessage && <p>{successMessage}</p>}
